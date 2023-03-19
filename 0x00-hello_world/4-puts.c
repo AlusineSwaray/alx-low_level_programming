797cad8eb49cf6185031562d86013c2a40b94e0d
Swaray <aswaray650@gmail.com>
2023-03-19 21:59:44 +0000

    boring

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a line of code
+ *
+ * Return: 0 unless otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
