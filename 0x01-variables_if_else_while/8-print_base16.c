765b9e47814deb0394ac3e9387945dccf3c0a6d4
Swaray <aswaray650@gmail.com>
2023-03-20 15:34:17 +0000

    trying to pull from git

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -hexadecimal printing %x %X
+ *
+ * Return: 0 unless otherwise
+ */
+int main(void)
+{
+	int i;
+	char hex;
+	
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (hex = 'a'; hex <= 'f'; hex++)
+	       putchar(hex);	
+	putchar('\n');
+	return (0);
+}
