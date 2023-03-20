9e2933a2c6a9e6e08e8f576cb9fce34b199a49f9
Swaray <aswaray650@gmail.com>
2023-03-20 00:28:12 +0000

    small addition

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,6 @@ int main(void)
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		if (alpha != 'q' && alpha != 'e')
 			putchar(alpha);
-	for (ALPHA = 'A'; ALPHA <= 'Z'; ALPHA++)
-		putchar(ALPHA);
 	putchar('\n');
 	return (0);
 }
