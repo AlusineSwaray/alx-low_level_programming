bf1a91cd71d20e1d9dcd56f60333e4200baef3d0
Alusine Swaray <alusineswaray08@gmail.com>
2023-03-20 07:40:49 -0700

    i did this on my phone

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		putchar(i + '0');
 		if (i == 9)
-			break;
-			putchar(',');
-				putchar(' ');
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
