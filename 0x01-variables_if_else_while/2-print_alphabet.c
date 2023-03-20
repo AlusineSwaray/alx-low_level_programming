5a890f43fe0f1936ac35ddfe7f1fc1ec28dd933a
Swaray <aswaray650@gmail.com>
2023-03-20 00:06:12 +0000

    i used ansi numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: 0 unless otherwise
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		printf("%c\n", i);
+	return (0);
+}
