304c2fbc7c7399f84779ab1d1b1ae4bf64e98c01
Swaray <aswaray650@gmail.com>
2023-03-23 02:23:40 +0000

    hii

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-void positive_or_negative();
-int _putchar(char c);
-void print_alphabet();
-void print_alphabet_x10();
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+void positive_or_negative(void);
+
+
 #endif
