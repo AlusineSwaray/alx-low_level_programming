43a6e378bdfecdb8ea05c65389c2675ddf6de2ba
Swaray <aswaray650@gmail.com>
2023-03-24 08:10:01 +0000

    sweet

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a line on screen
+ *@n: length of lines to be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int space;
+
+	while (i < n)
+	{
+		if (n == 0)
+			_putchar('\n');
+		for (space = 0; space < i; space++)/*prints space after the new line*/
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+
+}
