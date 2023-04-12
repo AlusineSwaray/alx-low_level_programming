0f9870176ae66341c22e642e3aee9e8958c3b82b
AlusineSwaray <alusineswaray08@gmail.com>
2023-04-12 23:07:24 +0000

    tired

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -18,10 +18,10 @@ void print_rev(char *rev)
 		rev++;/*increament the size of the pointer by itself*/
 	}
 
-	for (p = 1; p <= i; p++)
+	for (p = 0; p <= i; p++)
 	{
 		_putchar(*rev);
-		rev--;
+		--rev;
 	}
 	_putchar('\n');
 }
