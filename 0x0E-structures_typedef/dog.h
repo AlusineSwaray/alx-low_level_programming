ce4185da7432f0befea593c0eb4b79ea17d1e902
AlusineSwaray <alusineswaray08@gmail.com>
2023-06-21 03:12:05 +0000

    testing

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - A struct for info on dogs
+  * @name: name tag
+  * @age: age tag
+  * @owner: owner tag
+  *
+  * Description: lorem ipsum
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
