6e18b8d860330867d47c130379bde224818a934b
AlusineSwaray <alusineswaray08@gmail.com>
2023-06-23 17:32:06 +0000

    i am finally understanding this

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that sums up arguments
+ * @separator: size of array
+ * @n: numbers
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int num;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (*separator != '\0' && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
+
