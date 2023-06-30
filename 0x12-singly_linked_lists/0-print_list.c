c0200ae06aa5eae38a8482a2e08b42dd44e8c326
AlusineSwaray <alusineswaray08@gmail.com>
2023-06-30 12:47:09 +0000

    task 0 linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_list - prints a singly linked list
+ * @h: head of the linked list
+ *
+ * Return: the number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	list_t const *ptr = h;
+
+	if (h == NULL)
+		printf("[0] (nil)\n");
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+			count++;
+		}
+		ptr = ptr->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+I enjoyed Prof Richard Buckland's lecture on linked list
