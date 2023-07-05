af6c5d596a73e178198ad0ce2c9dacb56b0091a2
AlusineSwaray <alusineswaray08@gmail.com>
2023-07-05 20:07:35 +0000

    declare dunctions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,27 +2,30 @@
 #define DOG_H
 #include <stdio.h>
 
-
 /**
  * u_int - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
 /**
-  * struct dog - A struct for info on dogs
-  * @name: name tag
-  * @age: age tag
-  * @owner: owner tag
-  *
-  * Description: lorem ipsum
-  */
+ * struct dog - A struct for info on dogs
+ * @name: name tag
+ * @age: age tag
+ * @owner: owner tag
+ *
+ * Description: lorem ipsum
+ */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+  char *name;
+  float age;
+  char *owner;
 };
 
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcopy(char *dest, char *src);
+int _strlen(const char *str);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
